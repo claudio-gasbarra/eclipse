0fdd470545d8d191cde6ef8351538f66bcac3eb6
Claudio <claudio.gasbarra@gmail.com>
2021-04-17 18:06:22 +0200

    commit eclipse

---
+++ eclipse-workspace/geometria/src/test/java/FuncTest/FirefoxTest.java
@@ -0,0 +1,32 @@
+package FuncTest;
+
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import java.time.Duration;
+
+import org.junit.Test;
+
+public class FirefoxTest {
+
+	@Test
+	public void test() {
+	        WebDriver driver = new FirefoxDriver();
+	        WebDriverWait wait = new WebDriverWait(driver,10);
+	        try {
+	            driver.get("https://google.com/ncr");
+	            driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+	            WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
+	            System.out.println(firstResult.getAttribute("textContent"));
+	            } 
+	        finally {
+	            driver.quit();
+	                }
+	                   }
+
+                         }
